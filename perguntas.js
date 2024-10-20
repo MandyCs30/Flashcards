77be81226e5aaa8de1f45c21050806a1c7147c11
MandyCs30 <amandacarvalhos@prof.educacao.sp.gov.br>
2024-10-20 17:56:04 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,47 @@
+criaCartao(
+    'Números Racionais',
+    'Qual o valor da raiz quadrada de 64?',
+    'A raiz de 64 é 8.'
+)
+
+criaCartao(
+   'Teorema de Pitágoras',
+    'Qual o valor da hipotenusa, quando seus catetos for 3 e 4?',
+    'A hipotenusa é igual a 5.'
+)
+
+criaCartao(
+   'Função Quadrática',
+    'O que determina o coeficiente a da função de 2º grau?',
+    'Determina a concavidade da minha parábola. Se a > 0, a concavidade é voltada para cima. Se a < 0, a concavidade é voltada para baixo.'
+)
+criaCartao(
+    'Função Afim',
+    'Qual a representação gráfica de uma função de 1º grau?',
+    'A representação gráfica é uma reta.'
+) 
+criaCartao(
+    'Porcentagem',
+    'Quanto é 30% de 120?',
+    '30% de 120 é igual a 36.'
+) 
+criaCartao(
+    'Probabilidade',
+    'Qual a probabilidade de sair um número menor que 5 em um lançamento de um dado?',
+    'A probabilidade é de 2/3.'
+) 
+criaCartao(
+    'Grandezas Proporcionais',
+    'A quantidade de pedreiros e o tempo para terminar a construção de uma casa, são grandezas diretamente ou inversamente proporcionais?',
+    'São grandezas inversamente proporcionais, quanto maior a quantidade de pedreiros, menos tempo terminará a construção.'
+) 
+criaCartao(
+    'Medidas de Tendência Central',
+    'Dado os valores a seguir: 8,2,5,4,2,6,8,8,9 e 4, qual é o valor da moda?',
+    'O valor da moda é 8.'
+) 
+criaCartao(
+    'Progressão Geométrica',
+    'Dada a sequência (1, 3, 9, 27, 81,...), qual é o próximo termo?',
+    'O próximo termo da P.G. é 243.'
+) 
